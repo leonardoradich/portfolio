aa581b2b5b7945e3d633fa78e16caf2c390a350f
leo <896355@stud.unive.it>
2026-03-03 12:53:44 +0100

    Aggiornata Hero section e Sidebar

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+export const SITE_TITLE = 'Leonardo Radich| Data Science Developer';
+export const SITE_DESCRIPTION = 'Informatica & Data Science Student at the University of Ca Foscari. I am passionate about data science, machine learning, and software development. I am always eager to learn new technologies and improve my skills.';
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
